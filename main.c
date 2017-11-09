2b041262d0914d85de1fdb2d495ca995f260c601
Alexander Mason <masonal@ada.cs.pdx.edu>
2017-11-08 18:04:00 -0800

    This is a message

--- main.c
+++ main.c
@@ -6,34 +6,50 @@
 //      Written by:                       //
 //      Ben Portis and Alexander Mason    //
 //                                        //
-//                                        //
-//////////////////////////////////////////
+////////////////////////////////////////////
 
 #include <stdio.h>
 
+/*
+ * Adj List
+ * [A]: [B, 0-9] -> [C, +/-] -> [D, .] -> [DEAD, e/E]
+ * [B]: [B, 0-9] -> [F, e/E] -> [I, .] -> [DEAD, +/-]
+ * [C]: [B, 0-9] -> [D, .] -> [DEAD, e/E/+/-]
+ * [D]: 
+ * [E]
+ * [F]
+ * [G]
+ * [H]
+ * [I]:
+ * [DEAD]
+ */
 
-
-int main(void)
+int main(arg c, arg[v])
 {
 
-    static const char fileName[] = "input.txt";
-    FILE *file = fopen (fileName,"r");
-    if(file != NULL)
-    {
-        char linez[256];
-     
-        //read line by line 
-        while(fgets(linez, sizeof linez, file) != NULL) 
+    if(c){
+        static const char fileName[] = "input.txt";
+        FILE *file = fopen (fileName,"r");
+        if(file != NULL)
         {
-            //do all the things
-            
-            fputs(linez, stdout); //write out line by line
+            char linez[256];
+
+            //read line by line 
+            while(fgets(linez, sizeof linez, file) != NULL) 
+            {
+                //do all the things
+                
+                fputs(linez, stdout); //write out line by line
+            }
+            fclose(file);
         }
-        fclose(file);
-    }
-    else
+        else
+        {
+            perror(fileName);//if file didn't open
+        }
+    }else
     {
-        perror(fileName);//if file didn't open
+        printf("No file specified. Exiting...");
     }
 
     return 0;
