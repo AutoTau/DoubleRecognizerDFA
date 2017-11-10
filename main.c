3c7803bb935e4b0d6ddac748d19321e2b61bd1c1
Alexander Mason <masonal@ada.cs.pdx.edu>
2017-11-10 15:09:18 -0800

    Added first pass at logic for traversing graph. Need to implement checking each char in the file's string still.

--- main.c
+++ main.c
@@ -39,36 +39,51 @@ int main(int argc,  char * argv[])
         char * fileName = new char[strlen(tempName + 1)];
         FILE *file = fopen (fileName,"r");
 
-        if(file != NULL)
-        {
+        if(file != NULL){
             char linez[256];
 
             //read line by line 
-            while(fgets(linez, sizeof linez, file) != NULL) 
-            {
+            while(fgets(linez, sizeof linez, file) != NULL) {
                 //Index number indicating current state
                 int currState = 0;
-                //Create temp edge pointer
-                edge * current = NULL;
-                //Go to array index that matches the current state.
-                while(currState != 9){
-                    //Check if first node contain the grabbed char from string
-                    while(
-                            if(strstr(
-                                    //Go to the new array index, repeat
-                                    else
-                                    current = state.next;
-                                    if(state.accept)
-                                    printf("Accept");
-                                    if(!state.accept)
-                                    printf("Reject");
+                //Make sure we aren't dead
+                bool dead = false;
+                
+                //While we haven't hit the dead state an there are more chars
+                while(!dead && consume != '\n'){
+                    //Go to the beginning of the list of edges for current state
+                    Edge temp = machine[currState].head;
+
+                    //If we haven't reached the end of the edges in an edge list
+                    if(temp != NULL){
+                        //If I should follow this edge
+                        if(strstr(temp->cond, consume)){
+                            //Move to the new state after consuming
+                            currState = temp->index;
+                        }
+                        else{
+                            //Shouldn't follow edge, go to next edge
+                            temp = temp->next;
+                        }
+                    }
+                    else{
+                        //End of an edge list, hit dead state
+                        dead = true;
+                    }
+
+                    //If the current state is accepting and we aren't dead
+                    if(machine[currState].accepting && !dead){
+                        printf("Accept\n\n");
+                    }
+                    else{
+                        printf("Reject\n\n");
+                    }
                 }
                 fputs(linez, stdout); //write out line by line
             }
             fclose(file);
         }
-        else
-        {
+        else{
             perror(fileName);//if file didn't open
         }
     }else{
